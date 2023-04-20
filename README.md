80b3cd145ca64ea7c1cd9f9dcfda26341cc2d247
Jay Simons <jay@designly.biz>
2023-04-19 21:32:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# react-turnstile
+
+A very simple React library for [Cloudflare Turnstile](https://challenges.cloudflare.com).
+
+## Installation
+
+```sh
+npm i react-turnstile
+```
+
+## Usage
+
+```jsx
+import Turnstile, { useTurnstile } from "react-turnstile";
+
+// ...
+
+const { reset } = useTurnstile(); // Access turnstile reset method
+
+function TurnstileWidget() {
+  return (
+    <Turnstile
+      sitekey="1x00000000000000000000AA"
+      onVerify={(token) => alert(token)}
+    />
+  );
+}
+```
+
+Turnstile tokens expire after 5 minutes, to automatically reset the challenge once they expire,
+set the `autoResetOnExpire` prop to true or reset the widget yourself using the `onExpire` callback.
+
+## Documentation
+
+Turnstile takes the following arguments:
+
+| name              | type    | description                                           |
+| ----------------- | ------- | ----------------------------------------------------- |
+| sitekey           | string  | sitekey of your website (REQUIRED)                    |
+| action            | string  | -                                                     |
+| cData             | string  | -                                                     |
+| theme             | string  | one of "light", "dark", "auto"                        |
+| tabIndex          | number  | -                                                     |
+| responseField     | boolean | controls generation of `<input />` element            |
+| responseFieldName | string  | changes the name of `<input />` element               |
+| retry             | string  | one of "auto", "never"                                |
+| retryInterval     | number  | interval of retries in ms                             |
+| autoResetOnExpire | boolean | automatically reset the widget when the token expires |
+| id                | string  | id of the div                                         |
+| ref               | Ref     | custom react ref for the div                          |
+| className         | string  | passed to the div                                     |
+| style             | object  | passed to the div                                     |
+
+And the following callbacks:
+
+| name      | arguments | description                                |
+| --------- | --------- | ------------------------------------------ |
+| onVerify  | token     | called when challenge is passed (REQUIRED) |
+| onLoad    | widgetId  | called when the widget is loaded           |
+| onError   | error     | called when an error occurs                |
+| onExpire  | -         | called when the token expires              |
+| onTimeout | -         | called when the challenge expires          |
+
+For more details on what each argument does, see the [Cloudflare Documentation](https://developers.cloudflare.com/turnstile/get-started/client-side-rendering/#configurations).
---
+++ src/index.tsx
@@ -0,0 +1,183 @@
+import React, { useEffect, useState, useRef } from "react";
+import { TurnstileOptions, SupportedLanguages } from "turnstile-types";
+
+const global = (typeof globalThis !== "undefined" ? globalThis : window) as any;
+let turnstileState =
+  typeof global.turnstile !== "undefined" ? "ready" : "unloaded";
+let ensureTurnstile: () => Promise<any>;
+
+// Functions responsible for loading the turnstile api, while also making sure
+// to only load it once
+{
+  const TURNSTILE_LOAD_FUNCTION = "cf__reactTurnstileOnLoad";
+  const TURNSTILE_SRC = "https://challenges.cloudflare.com/turnstile/v0/api.js";
+
+  let turnstileLoad: {
+    resolve: (value?: any) => void;
+    reject: (reason?: any) => void;
+  };
+  const turnstileLoadPromise = new Promise((resolve, reject) => {
+    turnstileLoad = { resolve, reject };
+    if (turnstileState === "ready") resolve(undefined);
+  });
+
+  ensureTurnstile = () => {
+    if (turnstileState === "unloaded") {
+      turnstileState = "loading";
+      global[TURNSTILE_LOAD_FUNCTION] = () => {
+        turnstileLoad.resolve();
+        turnstileState = "ready";
+        delete global[TURNSTILE_LOAD_FUNCTION];
+      };
+      const url = `${TURNSTILE_SRC}?onload=${TURNSTILE_LOAD_FUNCTION}&render=explicit`;
+      const script = document.createElement("script");
+      script.src = url;
+      script.async = true;
+      script.addEventListener("error", () => {
+        turnstileLoad.reject("Failed to load Turnstile.");
+        delete global[TURNSTILE_LOAD_FUNCTION];
+      });
+      document.head.appendChild(script);
+    }
+    return turnstileLoadPromise;
+  };
+}
+
+export function useTurnstile() {
+  const reset = () => {
+    global.turnstile.reset();
+  }
+
+  return {
+    reset
+  }
+}
+
+export default function Turnstile({
+  id,
+  className,
+  style,
+  sitekey,
+  action,
+  cData,
+  theme,
+  language,
+  tabIndex,
+  responseField,
+  responseFieldName,
+  size,
+  retry,
+  retryInterval,
+  refreshExpired,
+  appearance,
+  execution,
+  userRef,
+  onVerify,
+  onLoad,
+  onError,
+  onExpire,
+  onTimeout,
+}: TurnstileProps) {
+  const ownRef = useRef<HTMLDivElement | null>(null);
+  const inplaceState = useState<TurnstileCallbacks>({ onVerify })[0];
+
+  const ref = userRef ?? ownRef;
+
+  useEffect(() => {
+    if (!ref.current) return;
+    let cancelled = false;
+    let widgetId = "";
+    (async () => {
+      // load turnstile
+      if (turnstileState !== "ready") {
+        try {
+          await ensureTurnstile();
+        } catch (e) {
+          inplaceState.onError?.(e);
+          return;
+        }
+      }
+      if (cancelled || !ref.current) return;
+      const turnstileOptions: TurnstileOptions = {
+        sitekey,
+        action,
+        cData,
+        theme,
+        language,
+        tabindex: tabIndex,
+        "response-field": responseField,
+        "response-field-name": responseFieldName,
+        size,
+        retry,
+        "retry-interval": retryInterval,
+        "refresh-expired": refreshExpired,
+        appearance,
+        execution,
+        callback: (token: string) => inplaceState.onVerify(token),
+        "error-callback": () => inplaceState.onError?.(),
+        "expired-callback": () => inplaceState.onExpire?.(),
+        "timeout-callback": () => inplaceState.onTimeout?.(),
+      };
+
+      widgetId = window.turnstile.render(ref.current, turnstileOptions);
+      inplaceState.onLoad?.(widgetId);
+    })();
+    return () => {
+      cancelled = true;
+      if (widgetId) window.turnstile.remove(widgetId);
+    };
+  }, [
+    sitekey,
+    action,
+    cData,
+    theme,
+    language,
+    tabIndex,
+    responseField,
+    responseFieldName,
+    size,
+    retry,
+    retryInterval,
+    refreshExpired,
+    appearance,
+    execution,
+  ]);
+  useEffect(() => {
+    inplaceState.onVerify = onVerify;
+    inplaceState.onLoad = onLoad;
+    inplaceState.onError = onError;
+    inplaceState.onExpire = onExpire;
+    inplaceState.onTimeout = onTimeout;
+  }, [onVerify, onLoad, onError, onExpire, onTimeout]);
+
+  return <div ref={ref} id={id} className={className} style={style} />;
+}
+
+interface TurnstileProps extends TurnstileCallbacks {
+  sitekey: string;
+  action?: string;
+  cData?: string;
+  theme?: "light" | "dark" | "auto";
+  language?: SupportedLanguages | "auto";
+  tabIndex?: number;
+  responseField?: boolean;
+  responseFieldName?: string;
+  size?: "normal" | "invisible" | "compact";
+  retry?: "auto" | "never";
+  retryInterval?: number;
+  refreshExpired?: "auto" | "manual" | "never";
+  appearance?: "always" | "execute" | "interaction-only";
+  execution?: "render" | "execute";
+  id?: string;
+  userRef?: React.MutableRefObject<HTMLDivElement>;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+interface TurnstileCallbacks {
+  onVerify: (token: string) => void;
+  onLoad?: (widgetId: string) => void;
+  onError?: (error?: Error | any) => void;
+  onExpire?: () => void;
+  onTimeout?: () => void;
+}
